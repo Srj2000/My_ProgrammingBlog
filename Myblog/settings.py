4adf2928be732ce81cd36970e15198c3b76a1523
Srj2000 <srjsngh7714@gmail.com>
2021-09-18 19:42:58 +0530

    first commit

--- Myblog/settings.py
+++ Myblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,5 +136,6 @@ EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_HOST_PASSWORD="YOUR PASSWORD"
 
-STATIC_ROOT= 'staticfiles'
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+django_heroku.settings(locals())
