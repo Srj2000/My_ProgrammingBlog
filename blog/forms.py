d5ceab0cf20fa6e51cf8a2adc59f6c6426fb8bda
Srj2000 <srjsngh7714@gmail.com>
2021-09-18 22:01:00 +0530

    templates

--- blog/forms.py
+++ blog/forms.py
@@ -7,7 +7,7 @@ from .models import Contact, Post
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.forms.fields import CharField, ImageField
 
-from django.forms.widgets import EmailInput, FileInput, HiddenInput, NumberInput, PasswordInput, TextInput,TextInput
+from django.forms.widgets import EmailInput, FileInput, HiddenInput, NumberInput, PasswordInput, TextInput,TextInput, Textarea
 
 
 # class Contactform(forms.Form):
@@ -36,8 +36,8 @@ class PostForm(forms.ModelForm):
         model = Post
         exclude=["p_author"]
 
-        widgets = { "p_title": TextInput(attrs={"class": "form-control"}), "p_body":TextInput(attrs={"class": "form-control  body", }),
-                   "p_head1": TextInput(attrs={"class": "form-control"}), "p_chead1":TextInput(attrs={"class": "form-control head1", }), "p_head2": TextInput(attrs={"class": "form-control"}), "p_chead2":TextInput(attrs={"class": "form-control head2", }), "p_thumbnail":FileInput(attrs={"class": "form-control", }),  }
+        widgets = { "p_title": TextInput(attrs={"class": "form-control"}), "p_body":Textarea(attrs={"class": "form-control  body", }),
+                   "p_head1": TextInput(attrs={"class": "form-control"}), "p_chead1":Textarea(attrs={"class": "form-control head1", }), "p_head2": TextInput(attrs={"class": "form-control"}), "p_chead2":Textarea(attrs={"class": "form-control head2", }), "p_thumbnail":FileInput(attrs={"class": "form-control", }),  }
                    
         labels = {"p_title": "Title", "p_body": "Body", "p_head1": "Head", "p_chead1": "Head Content",
                   "p_head2": "Head 2", "p_chead2": "Head 2 Content", "p_thumbnail": "Image"}
